1a174bd6960311284698142e0df88309de4d0836
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-04-07 14:56:09 -0300

    MetricCollector
    Signed off by: Renan Fichberg <rfichberg@gmail.com>

---
+++ app/models/metric_collector.rb
@@ -0,0 +1,9 @@
+class MetricCollector
+  def name; raise NotImplementedError; end
+
+  def description; raise NotImplementedError; end
+
+  def supported_metrics; raise NotImplementedError; end
+
+  def collect_metrics(code_directory, wanted_metrics, result_writer); raise NotImplementedError; end
+end
---
+++ spec/models/metric_collector_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe MetricCollector do
+  describe 'method' do
+    describe 'name' do
+      it 'should raise a NotImplementedError' do
+        expect { subject.name }.to raise_error(NotImplementedError)
+      end
+    end
+
+    describe 'description' do
+      it 'should raise a NotImplementedError' do
+        expect { subject.description }.to raise_error(NotImplementedError)
+      end
+    end
+
+    describe 'supported_metrics' do
+      it 'should raise a NotImplementedError' do
+        expect { subject.supported_metrics }.to raise_error(NotImplementedError)
+      end
+    end
+
+    describe 'collect_metrics' do
+      it 'should raise a NotImplementedError' do
+        expect { subject.collect_metrics("", "", "") }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+end
