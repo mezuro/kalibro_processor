35e7cf08d1830d082817bae9c649c1457367ea86
Renan Fichberg <rfichberg@gmail.com>
2014-04-11 15:26:54 -0300

    WIP

--- app/models/module_result.rb
+++ app/models/module_result.rb
@@ -1,4 +1,17 @@
 class ModuleResult
-  attr_reader :id, :grade, :hieght, :parent
+  attr_reader :id, :grade, :height, :parent
   attr_accessor :children
+
+  def initialize(id, grade, height, parent, children)
+    @id = id
+    @grade = grade
+    @height = height
+    @parent = parent
+    @children = children
+  end
+
+  def has_parent?
+    @parent != nil
+  end
+
 end
