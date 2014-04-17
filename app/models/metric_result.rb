e0457c0b782704ad0b088b46518539bd50458362
Diego Araújo <diegoamc90@gmail.com>
2014-04-17 12:19:38 -0300

    aggregated_value method.
    Now using singleton method for descendant_results instance.
    Using descriptive_statistics to calculate aggregated_values.
    
    signed-off-by: Alessandro Palmeira <alessandro.palmeira@gmail.com>
    signed-off-by: Renan Fichberg <rfichberg@gmail.com>

--- app/models/metric_result.rb
+++ app/models/metric_result.rb
@@ -7,11 +7,21 @@ class MetricResult
     @metric_configuration = metric_configuration
     @value = value
     @error = error
+    @descendant_results = DescriptiveStatistics::Stats.new(Array.new)
+    def @descendant_results.average
+      self.mean
+    end
   end
 
   def has_error?; !@error.nil?; end
 
-  def aggregated_value; raise NotImplementedError; end
+  def aggregated_value
+    if (@value.nan? && !@descendant_results.empty?)
+      @descendant_results.send( @metric_configuration.aggregation_form.to_s.downcase )
+    else
+      @value
+    end
+  end
 
   def has_range?; !@metric_configuration.range.nil?; end
 
