fb1879b487f26abf534130548a8e6ccff2086dde
Renan Fichberg <rfichberg@gmail.com>
2014-04-25 14:24:26 -0300

    Initialize method w/ AbstractModuleResult fields.

--- app/models/module_result.rb
+++ app/models/module_result.rb
@@ -1,17 +1,19 @@
 class ModuleResult
-  attr_reader :id, :grade, :height, :parent
-  attr_accessor :children
+  attr_reader :id, :grade, :height, :parent, :kalibro_module
+  attr_accessor :children, :metric_results
 
-  def initialize(id, grade, height, parent, children)
+  def initialize(id, grade, height, parent, children, kalibro_module)
     @id = id
     @grade = grade
     @height = height
     @parent = parent
     @children = children
+    @kalibro_module = kalibro_module
+    @metric_configuration = KalibroGatekeeperClient::Entities::MetricConfiguration.new
+    @metric_results = [MetricResult.new(metric_configuration, value, nil)]
   end
 
   def has_parent?
     @parent != nil
   end
-
 end
