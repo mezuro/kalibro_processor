cabe54c954580616e4b01cfbfcb3515fbf320134
Diego Araújo <diegoamc90@gmail.com>
2014-05-07 13:17:21 -0300

    Removed some tests.
    There were some tests for ModuleResult that didn't make sense anymore.
    
    signed-off-by: Rafael Manzo <rr.manzo@gmail.com>
    signed-off-by: Renan Fichberg <rfichberg@gmail.com>

--- spec/models/module_result_spec.rb
+++ spec/models/module_result_spec.rb
@@ -45,49 +45,6 @@ describe ModuleResult do
   end
 
   describe 'method' do
-    describe 'initialize' do
-      before :each do
-        KalibroGatekeeperClient::Entities::MetricConfiguration.expects(:find).
-          at_least_once.returns(FactoryGirl.build(:metric_configuration))
-      end
-
-      context 'with valid attributes' do
-        let(:my_parent) { FactoryGirl.build(:module_result) }
-        let(:kalibro_module) { FactoryGirl.build(:kalibro_module) }
-
-        subject { FactoryGirl.build(:module_result, parent: my_parent) }
-
-        it 'should return an instance of ModuleResult' do
-          subject.should be_a(ModuleResult)
-        end
-
-        it 'should have the right attributes' do
-          subject.parent.should eq(my_parent)
-          subject.height.should eq(0)
-          subject.children.all.should be_empty
-        end
-      end
-    end
-
-    describe 'children' do
-      before :each do
-        KalibroGatekeeperClient::Entities::MetricConfiguration.expects(:find).
-          at_least_once.returns(FactoryGirl.build(:metric_configuration))
-      end
-
-      context 'when a module result has children' do
-        let(:child_module_result) { FactoryGirl.build(:module_result) }
-        let(:parent_module_result) { FactoryGirl.build(:module_result, children: [child_module_result]) }
-
-        it 'should set the children parents' do
-          child_module_result.parent = parent_module_result
-
-          parent_module_result.children.should eq([child_module_result])
-          child_module_result.parent.should eq(parent_module_result)
-        end
-      end
-    end
-
     describe 'metric_result_for' do
       before :each do
         KalibroGatekeeperClient::Entities::MetricConfiguration.expects(:find).
@@ -107,20 +64,6 @@ describe ModuleResult do
         it 'should return the metric_result' do
           subject.metric_result_for(another_metric).should be_nil
         end
-      end
-    end
-
-    describe 'add metric_result (not a method)' do
-      subject { FactoryGirl.build(:module_result, metric_results: []) }
-      let(:metric_result) { FactoryGirl.build(:metric_result) }
-
-      before :each do
-        KalibroGatekeeperClient::Entities::MetricConfiguration.expects(:find).at_least_once.returns(FactoryGirl.build(:metric_configuration))
-      end
-
-      it 'should add a metric_result using <<' do
-        subject.metric_results << metric_result
-        subject.metric_results.should include(metric_result)
       end
     end
   end
