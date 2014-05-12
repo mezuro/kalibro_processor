12577c4ae4172e6f770624cb20ee051b3892d587
Fellipe Souto <fllsouto@gmail.com>
2014-05-12 17:32:21 -0300

    I've started to write task #6
    Pending unit test and the other methods of this class

--- app/models/analizo_metric_collector.rb
+++ app/models/analizo_metric_collector.rb
@@ -1,9 +1,26 @@
 class AnalizoMetricCollector < MetricCollector
-  attr_reader :description, :supported_metrics
+  attr_reader :description, :supported_metrics, :wanted_metrics
 
   def initialize
-    @supported_metrics = Hash.new
     @description = YAML.load_file('config/collectors_descriptions.yml')["analizo"]
+    @supported_metrics = parse_supported_metrics
+  end
+
+  def result_parser(wanted_metrics)
+    self.wanted_metrics = Hash.new
+    @supported_metrics.each_key do |code|
+      if wanted_metrics.include?(@supported_metrics[code])
+        self.wanted_metrics.store(code, @supported_metrics[code])
+      end
+    end
+  end
+
+  def metric_result(code, result)
+    native_metric_result(@wanted_metric[code], result.to_f)
+  end
+
+  def native_metric_result(code,result)
+        raise NotImplementedError.new    
   end
 
   def name
@@ -15,15 +32,16 @@ class AnalizoMetricCollector < MetricCollector
   end
 
   def parse_supported_metrics
+    supported_metrics = Hash.new
     analizo_metric_list = metric_list
     analizo_metric_list.each_line do |line|
       if line.include?("-")
         code = line[/^[^ ]*/] # From the beginning of line to the first space
         name = line[/- .*$/].slice(2..-1) # After the "- " to the end of line
         scope = code.start_with?("total") ? :SOFTWARE : :CLASS
-        @supported_metrics[code] = Metric.new(false, name, scope)
+        supported_metrics[code] = Metric.new(false, name, scope)
       end
     end
-    @supported_metrics
+    supported_metrics
   end
 end
