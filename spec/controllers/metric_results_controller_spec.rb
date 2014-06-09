9a76f1d705abac03974d3c048f09182a7fa3efda
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-06-09 13:59:38 -0300

    MetricResults#descendant_values

--- spec/controllers/metric_results_controller_spec.rb
+++ spec/controllers/metric_results_controller_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe MetricResultsController do
   describe 'method' do
     describe 'descendant_values' do
-      let!(:metric_result) { FactoryGirl.build(:metric_result) }
+      let!(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
+      let!(:module_result) { FactoryGirl.build(:module_result) }
 
       before :each do
-        metric_result
-        MetricResult.expects(:find).with().returns(metric_result)
+        module_result.expects(:metric_result_for).with(metric_result.metric).returns(metric_result)
+        module_result.expects(:children).returns([module_result])
+        metric_result.expects(:module_result).returns(module_result)
+        MetricResult.expects(:find).with(metric_result.id.to_s).returns(metric_result)
       end
 
       context 'json format' do
