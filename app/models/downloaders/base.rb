4bf3b85b7bffa72751091cad57c488f6460efff9
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-06-15 11:39:49 -0300

    Downloaders::Base#retrieve! (pending a test case)

--- app/models/downloaders/base.rb
+++ app/models/downloaders/base.rb
@@ -1,5 +1,16 @@
 module Downloaders
   class Base
     def self.available?; raise NotImplementedError; end
+
+    def self.retrieve!(address, directory)
+      Dir.delete(directory) if Dir.exist?(directory) && !updatable?
+      get(address, directory)
+    end
+
+    protected
+
+    def self.get(address, directory); raise NotImplementedError; end
+
+    def self.updatable?; raise NotImplementedError; end
   end
 end
--- spec/models/downloaders/base_spec.rb
+++ spec/models/downloaders/base_spec.rb
@@ -7,5 +7,33 @@ describe Downloaders::Base, :type => :model do
         expect{ subject.class.available? }.to raise_error(NotImplementedError)
       end
     end
+
+    describe 'retrieve!' do
+      let(:directory) { "/tmp/test" }
+      let(:address) { "http://test.test" }
+
+      context 'when directory exists' do
+        before :each do
+          Dir.expects(:exist?).with(directory).returns(true)
+        end
+
+        it 'is expected to raise a NotImplementedYetError' do
+          expect{ subject.class.retrieve!(address, directory) }.to raise_error(NotImplementedError)
+        end
+
+        context "and it isn't updatable" do
+          before :each do
+            self.class.expects(:updatable?).returns(false)
+          end
+
+          it 'is expected to delete the directory and get (so raising a error)' do
+            pending "probably the updatable? mock doesn't work"
+            Dir.expects(:delete).with(directory).returns(0)
+
+            expect{ subject.class.retrieve!(address, directory) }.to raise_error(NotImplementedError)
+          end
+        end
+      end
+    end
   end
 end
