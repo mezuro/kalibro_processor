75ea8bc6fc83bd8500d06e3f51b978510ab2403b
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-06-16 12:20:03 -0300

    module_results_controller_spec syntax fixed and whitespaces removed

--- spec/controllers/module_results_controller_spec.rb
+++ spec/controllers/module_results_controller_spec.rb
@@ -4,7 +4,7 @@ describe ModuleResultsController do
   describe 'method' do
     describe 'get' do
       let!(:module_result) { FactoryGirl.build(:module_result, id: 1) }
-      
+
       context 'with valid ModuleResult instance' do
         before :each do
         ModuleResult.expects(:find).with(module_result.id.to_s).returns(module_result)
@@ -12,24 +12,23 @@ describe ModuleResultsController do
         end
 
         it { should respond_with(:success) }
-        
-        it 'should return the module_result' do
-          JSON.parse(response.body).should eq(JSON.parse(module_result.to_json))
-        end
 
+        it 'should return the module_result' do
+          expect(JSON.parse(response.body)).to eq(JSON.parse(module_result.to_json))
+        end
       end
-      
+
       context 'with invalid ModuleResult instance' do
-        let!(:error_hash) { {error: 'RecordNotFound'} } 
+        let!(:error_hash) { {error: 'RecordNotFound'} }
         before :each do
         ModuleResult.expects(:find).with(module_result.id.to_s).returns(error_hash)
           post :get, id: module_result.id, format: :json
         end
-        
-        it { should respond_with(:unprocessable_entity) }
-        
+
+        it { is_expected.to respond_with(:unprocessable_entity) }
+
         it 'should return the error_hash' do
-          JSON.parse(response.body).should eq(JSON.parse(error_hash.to_json))
+          expect(JSON.parse(response.body)).to eq(JSON.parse(error_hash.to_json))
         end
       end
     end
