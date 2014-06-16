49d3c10e139f421f2199a8fa5ff81b0d8de6b5bc
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-06-16 12:28:30 -0300

    Removed unnecessary let! and using rails_helper

--- spec/controllers/module_results_controller_spec.rb
+++ spec/controllers/module_results_controller_spec.rb
@@ -1,17 +1,18 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe ModuleResultsController do
   describe 'method' do
     describe 'get' do
-      let!(:module_result) { FactoryGirl.build(:module_result, id: 1) }
+      let(:module_result) { FactoryGirl.build(:module_result, id: 1) }
 
       context 'with valid ModuleResult instance' do
         before :each do
-        ModuleResult.expects(:find).with(module_result.id.to_s).returns(module_result)
+          ModuleResult.expects(:find).with(module_result.id.to_s).returns(module_result)
+
           post :get, id: module_result.id, format: :json
         end
 
-        it { should respond_with(:success) }
+        it { is_expected.to respond_with(:success) }
 
         it 'should return the module_result' do
           expect(JSON.parse(response.body)).to eq(JSON.parse(module_result.to_json))
@@ -20,6 +21,7 @@ describe ModuleResultsController do
 
       context 'with invalid ModuleResult instance' do
         let!(:error_hash) { {error: 'RecordNotFound'} }
+
         before :each do
         ModuleResult.expects(:find).with(module_result.id.to_s).returns(error_hash)
           post :get, id: module_result.id, format: :json
