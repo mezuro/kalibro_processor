2f3e5669c12ae2654fe06732705018f665384f1d
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-06-23 22:37:23 -0300

    Removed unecessary get_state method from Processing class

--- app/models/processing.rb
+++ app/models/processing.rb
@@ -1,10 +1,4 @@
 class Processing < ActiveRecord::Base
   belongs_to :repository
   has_many :process_times
-
-  def get_state(process_state)
-    if state == 'ERROR' then raise NotImplementedError end
-    process_state
-  end
-
 end
