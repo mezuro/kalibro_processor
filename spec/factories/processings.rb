5ace0bffb68c185f6df519da0f6294c9e1150ec9
Diego Araújo <diegoamc@gmail.com>
2014-06-25 15:36:17 -0300

    Fixed processing factory
    signed-off-by: Rafael Reggiani Manzo <rr.manzo@gmail.com>

--- spec/factories/processings.rb
+++ spec/factories/processings.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     id "31"
     created_at "2011-10-20T18:26:43.151+00:00"
     state "READY"
-    process_time {[FactoryGirl.build(:process_time)]}
-    results_root_id "13"
+    process_times {[FactoryGirl.build(:process_time)]}
+    root_module_result_id 13
     repository { FactoryGirl.build(:repository) }
   end
 end
