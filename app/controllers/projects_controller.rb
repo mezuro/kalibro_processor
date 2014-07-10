92d8db88aa0851ed03b26699b9a7d9c323be92f0
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-07-10 11:56:49 -0300

    ProjectsController#get

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+  def all
+    projects = {projects: Project.all}
+
+    respond_to do |format|
+      format.json { render json: projects }
+    end
+  end
+
+  def get
+    begin
+      set_project
+      response = {project: @project}
+      status = :ok
+    rescue ActiveRecord::RecordNotFound
+      response = {error: 'RecordNotFound'}
+      status = :unprocessable_entity
+    end
+
+    respond_to do |format|
+      format.json { render json: response, status: status }
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id].to_i)
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, :type => :controller do
+  let(:project) { FactoryGirl.build(:project) }
 
+  describe 'all' do
+    let!(:projects) { [project] }
+
+    before :each do
+      Project.expects(:all).returns(projects)
+
+      get :all, format: :json
+    end
+
+    it { is_expected.to respond_with(:success) }
+
+    it 'is expected to return the list of projects converted to JSON' do
+      expect(JSON.parse(response.body)).to eq(JSON.parse({projects: projects}.to_json))
+    end
+  end
+
+  describe 'get' do
+    context 'when the Project exists' do
+      before :each do
+        Project.expects(:find).with(project.id).returns(project)
+
+        get :get, id: project.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:success) }
+
+      it 'is expected to return the list of projects converted to JSON' do
+        expect(JSON.parse(response.body)).to eq(JSON.parse({project: project}.to_json))
+      end
+    end
+
+    context 'when the Project exists' do
+      before :each do
+        Project.expects(:find).with(project.id).raises(ActiveRecord::RecordNotFound)
+
+        get :get, id: project.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:unprocessable_entity) }
+
+      it 'should return the error description' do
+        expect(JSON.parse(response.body)).to eq(JSON.parse({error: 'RecordNotFound'}.to_json))
+      end
+    end
+  end
 end
