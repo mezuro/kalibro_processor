6fcf90c5d665e70a71a0e18d5de277c6d8ff4555
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-07-16 16:05:55 -0300

    CompoundMetric Calculator
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/models/compound_results/calculator.rb
+++ app/models/compound_results/calculator.rb
@@ -1,15 +1,25 @@
 module CompoundResults
-  class calculator
-    def initialize(root_module_result, native_metrics, compound_metrics)
-      @current_module_result = root_module_result
-      @native_metrics = native_metrics
-      @compound_metrics = compound_metrics
-
+  class Calculator
+    def initialize(module_result, compound_metric_configurations)
+      @module_result = module_result
+      @compound_metric_configurations = compound_metric_configurations
     end
 
     def calculate
       evaluator = JavascriptEvaluator.new
 
+      @module_result.metric_results.each { |metric_result| evaluator.add_variable(metric_result.metric_configuration.code, metric_result.value) }
+
+      @compound_metric_configurations.each do |compound_metric_configuration|
+        evaluator.add_function(compound_metric_configuration.code, compound_metric_configuration.metric.script)
+      end
+
+      @compound_metric_configurations.each do |compound_metric_configuration|
+        MetricResult.create(metric: compound_metric_configuration.metric,
+                            module_result: @module_result,
+                            metric_configuration_id: compound_metric_configuration.id,
+                            value: evaluator.evaluate(compound_metric_configuration.code))
+      end
     end
   end
 end
---
+++ spec/models/compound_results/calculator_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe CompoundResults::Calculator, :type => :model do
+  describe 'calculate' do
+    let! (:module_result) { FactoryGirl.build(:module_result) }
+    let! (:metric_result) { FactoryGirl.build(:metric_result) }
+    let! (:compound_metric_configuration) { FactoryGirl.build(:compound_metric_configuration) }
+    let! (:metric_configuration) { FactoryGirl.build(:metric_configuration) }
+    let! (:value) { 13.0 }
+
+    before :each do
+      CompoundResults::JavascriptEvaluator.any_instance.expects(:add_variable).with(metric_configuration.code, metric_result.value)
+      CompoundResults::JavascriptEvaluator.any_instance.expects(:add_function).with(compound_metric_configuration.code, compound_metric_configuration.metric.script)
+      CompoundResults::JavascriptEvaluator.any_instance.expects(:evaluate).with(compound_metric_configuration.code).returns(value)
+      metric_result.expects(:metric_configuration).returns(metric_configuration)
+      module_result.expects(:metric_results).returns([metric_result])
+    end
+
+    it 'is expected to create a new MetricResult' do
+      MetricResult.expects(:create).with(metric: compound_metric_configuration.metric,
+                                         module_result: module_result,
+                                         metric_configuration_id: compound_metric_configuration.id,
+                                         value: value)
+
+      CompoundResults::Calculator.new(module_result, [compound_metric_configuration]).calculate
+    end
+  end
+end
