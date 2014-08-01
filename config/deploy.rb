8d75ee7ae02c0fefb13abf10918fa66d1e1625c9
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2014-08-01 16:58:21 -0300

    Added configuration to restart delayed_job worker.
    Signed off by: Rafael Manzo <rr.manzo@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'delayed_job:restart'
     end
   end
 
