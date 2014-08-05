e188808cd23c18efc4fb3b71c728d0a60a791c4b
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-08-05 12:02:57 -0300

    ProcessTime JSON includes the time

--- app/models/process_time.rb
+++ app/models/process_time.rb
@@ -4,7 +4,18 @@ class ProcessTime < ActiveRecord::Base
   attr_accessor :time
 
   def time
-    self.updated_at.to_datetime - self.created_at.to_datetime
+    unless self.created_at.nil?
+      self.updated_at.to_datetime - self.created_at.to_datetime
+    else
+      nil
+    end
   end
 
+  # Adding time
+  def to_json(options={})
+    json = super(options)
+    hash = JSON.parse(json)
+    hash["time"] = self.time
+    hash.to_json
+  end
 end
--- spec/models/process_time_spec.rb
+++ spec/models/process_time_spec.rb
@@ -17,5 +17,18 @@ RSpec.describe ProcessTime, :type => :model do
         expect( subject.time ).to eq(process_time)
       end
     end
+
+    describe "to_json" do
+      subject { FactoryGirl.build(:process_time) }
+
+      it 'is expected to add the time to the JSON' do
+        process_time = subject.updated_at.to_datetime - subject.created_at.to_datetime
+        expect(JSON.parse(subject.to_json)['time']).to eq(process_time.to_s)
+      end
+
+      it 'is expected to preserve all the attributes' do
+        expect(JSON.parse(subject.to_json)['state']).to eq(JSON.parse(subject.to_json)['state'])
+      end
+    end
   end
 end
