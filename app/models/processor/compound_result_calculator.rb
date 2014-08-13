5bca66f439a7bfee8e2fbc08b0364c6a4a52f1d6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2014-08-13 10:46:45 -0300

    Tests for compound result calculator.

--- app/models/processor/compound_result_calculator.rb
+++ app/models/processor/compound_result_calculator.rb
@@ -1,13 +1,13 @@
 module Processor
   class CompoundResultCalculator
 
-    def self.calculate_compound_results(module_result, compound_metrics)
+    def self.calculate_compound_results(module_result, compound_metric_configurations)
       unless module_result.children.empty?
-        module_result.children.each { |child| calculate_compound_results(child, compound_metrics) }
+        module_result.children.each { |child| calculate_compound_results(child, compound_metric_configurations) }
       end
 
       #TODO: there might exist the need to check the scope before trying to calculate
-      CompoundResults::Calculator.new(module_result, compound_metrics).calculate
+      CompoundResults::Calculator.new(module_result, compound_metric_configurations).calculate
     end
   end
 end
