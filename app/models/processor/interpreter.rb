e73a0df09869e02f257be9dc8c444d9f1578f103
Heitor Reis <marcheing@gmail.com>
2014-08-15 15:42:27 -0300

    Refactored interpreter to use the subtree method
    Signed off by: Diego Araujo <diegoamc90@gmail.com>

--- app/models/processor/interpreter.rb
+++ app/models/processor/interpreter.rb
@@ -1,21 +1,19 @@
 module Processor
   class Interpreter
     def self.interpret(module_result)
-      children = module_result.children
-      unless children.empty?
-        children.each { |child| interpret(child) }
+      all_module_results = module_result.subtree_elements
+      all_module_results.each do | module_result_child |
+        numerator = 0
+        denominator = 0
+        module_result_child.metric_results.each do |metric_result|
+          weight = metric_result.metric_configuration.weight
+          grade = metric_result.has_grade? ? metric_result.range.reading.grade : 0
+          numerator += weight*grade
+          denominator += weight
+        end
+        quotient = denominator == 0 ? 0 : numerator/denominator
+        module_result_child.update(grade: quotient)
       end
-
-      numerator = 0
-      denominator = 0
-      module_result.metric_results.each do |metric_result|
-        weight = metric_result.metric_configuration.weight
-        grade = metric_result.has_grade? ? metric_result.range.reading.grade : 0
-        numerator += weight*grade
-        denominator += weight
-      end
-      quotient = denominator == 0 ? 0 : numerator/denominator
-      module_result.update(grade: quotient)
     end
   end
 end
