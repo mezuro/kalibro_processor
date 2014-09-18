98314d4adc6e4b4b58798668a58439d9f800bf89
Diego Araújo <diegoamc90@gmail.com>
2014-09-18 09:46:29 -0300

    Using ActiveJob native error handling
    Signed off by: Rafael Reggiani Manzo <rr.manzo@gmail.com>

--- app/jobs/processing_job.rb
+++ app/jobs/processing_job.rb
@@ -8,26 +8,28 @@ class ProcessingJob < ActiveJob::Base
     @context = Processor::Context.new
   end
 
+  rescue_from(Errors::ProcessingCanceledError) do
+    @context.processing.destroy
+  end
+
+  rescue_from(Errors::ProcessingError) do |exception|
+    @context.processing.update(state: 'ERROR', error_message: exception.message)
+  end
+
   def perform(repository, processing)
     @context.repository = repository
     @context.processing = processing
 
-    begin
-      Processor::Preparer.perform(@context)
-      Processor::Downloader.perform(@context)
-      Processor::Collector.perform(@context)
-      Processor::TreeBuilder.perform(@context)
-      Processor::Aggregator.perform(@context)
-      Processor::CompoundResultCalculator.perform(@context)
-      Processor::Interpreter.perform(@context)
+    Processor::Preparer.perform(@context)
+    Processor::Downloader.perform(@context)
+    Processor::Collector.perform(@context)
+    Processor::TreeBuilder.perform(@context)
+    Processor::Aggregator.perform(@context)
+    Processor::CompoundResultCalculator.perform(@context)
+    Processor::Interpreter.perform(@context)
 
-      @context.processing.update(state: "READY")
-    rescue Errors::ProcessingCanceledError
-      @context.processing.destroy
-    rescue Errors::ProcessingError => error
-      @context.processing.update(state: 'ERROR', error_message: error.message)
+    @context.processing.update(state: "READY")
     rescue Errors::EmptyModuleResultsError
       self.processing.update(state: "READY")
-    end
   end
 end
