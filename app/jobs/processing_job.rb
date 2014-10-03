c352bdbcede4c4a36dfbfa121bd43466c84d8e2c
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2014-10-03 17:00:28 -0300

    [WIP] Working on periodic_processing
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/jobs/processing_job.rb
+++ app/jobs/processing_job.rb
@@ -8,6 +8,17 @@ class ProcessingJob < ActiveJob::Base
     @context = Processor::Context.new
   end
 
+#FIXME This should work for periodic_processing
+=begin
+  after_perform do |job|
+    period = @context.repository.period
+    if period > 0
+      new_processing = Processing.create(repository: @repository, state: "PREPARING")
+      ProcessingJob.set(wait: period.day).perform_later(@context.repository,new_processing)
+    end
+  end
+=end
+
   rescue_from(Errors::ProcessingCanceledError) do
     @context.processing.destroy
   end
