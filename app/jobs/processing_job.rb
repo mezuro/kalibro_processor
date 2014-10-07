c5ae22ec590edca52bf8d55a110324793691d0c1
Daniel Alves <danpaulalves@gmail.com>
2014-10-07 17:53:08 -0300

    Periodic processing enabled.
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/jobs/processing_job.rb
+++ app/jobs/processing_job.rb
@@ -8,16 +8,13 @@ class ProcessingJob < ActiveJob::Base
     @context = Processor::Context.new
   end
 
-#FIXME This should work for periodic_processing
-=begin
   after_perform do |job|
     period = @context.repository.period
     if period > 0
-      new_processing = Processing.create(repository: @repository, state: "PREPARING")
-      ProcessingJob.set(wait: period.day).perform_later(@context.repository,new_processing)
+      new_processing = Processing.create(repository: @context.repository, state: "PREPARING")
+      ProcessingJob.set(wait: period.days).perform_later(@context.repository, new_processing)
     end
   end
-=end
 
   rescue_from(Errors::ProcessingCanceledError) do
     @context.processing.destroy
