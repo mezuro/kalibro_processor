e18b6c1d3001c460a306ed686f496338d99b4295
Heitor Reis <marcheing@gmail.com>
2014-12-11 15:09:46 -0200

    Remaining pending tests waiting for a fix on ActiveResource
    Signed off by: Daniel Alves <danpaulalves@gmail.com>

--- spec/controllers/metric_results_controller_spec.rb
+++ spec/controllers/metric_results_controller_spec.rb
@@ -1,74 +1,74 @@
 require 'rails_helper'
 
 describe MetricResultsController do
-  pending 'kalibro client integration' do
-    describe 'method' do
-      describe 'descendant_values' do
-        let!(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
-        let!(:module_result) { FactoryGirl.build(:module_result) }
+  describe 'method' do
+    describe 'descendant_values' do
+      let!(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
+      let!(:module_result) { FactoryGirl.build(:module_result) }
 
-        before :each do
-          module_result.expects(:metric_result_for).with(metric_result.metric).returns(metric_result)
-          module_result.expects(:children).returns([module_result])
-          metric_result.expects(:module_result).returns(module_result)
-          MetricResult.expects(:find).with(metric_result.id.to_s).returns(metric_result)
-        end
-
-        context 'json format' do
-          before :each do
-            post :descendant_values, id: metric_result.id, format: :json
-          end
-
-          it { is_expected.to respond_with(:success) }
-
-          it 'returns the list of values' do
-            expect(JSON.parse(response.body)).to eq(JSON.parse({descendant_values: [metric_result.value]}.to_json))
-          end
-        end
+      before :each do
+        module_result.expects(:metric_result_for).with(metric_result.metric).returns(metric_result)
+        module_result.expects(:children).returns([module_result])
+        metric_result.expects(:module_result).returns(module_result)
+        MetricResult.expects(:find).with(metric_result.id.to_s).returns(metric_result)
       end
-    end
 
-    describe 'repository_id' do
-      let(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
-      let(:processing) { FactoryGirl.build(:processing) }
-      let(:repository) { FactoryGirl.build(:repository, id: 2) }
-
-      context 'with valid ModuleResult instance' do
+      context 'json format' do
         before :each do
-          MetricResult.expects(:find).with(metric_result.id.to_s).returns(metric_result)
-          metric_result.expects(:processing).returns(processing)
-          processing.expects(:repository).returns(repository)
-          post :repository_id, id: metric_result.id, format: :json
+          post :descendant_values, id: metric_result.id, format: :json
         end
 
         it { is_expected.to respond_with(:success) }
 
-        it 'should return the repository_id' do
-          expect(JSON.parse(response.body)).to eq(JSON.parse({repository_id: repository.id}.to_json))
-        end
-      end
-
-      context 'with invalid ModuleResult instance' do
-        let(:error_hash) { {error: 'RecordNotFound'} }
-
-        before :each do
-          MetricResult.expects(:find).with(metric_result.id.to_s).raises(ActiveRecord::RecordNotFound)
-
-          post :repository_id, id: metric_result.id, format: :json
-        end
-
-        it { is_expected.to respond_with(:unprocessable_entity) }
-
-        it 'should return the error_hash' do
-          expect(JSON.parse(response.body)).to eq(JSON.parse(error_hash.to_json))
+        it 'returns the list of values' do
+          expect(JSON.parse(response.body)).to eq(JSON.parse({descendant_values: [metric_result.value]}.to_json))
         end
       end
     end
+  end
 
-    describe 'metric_configuration' do
-      let!(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
+  describe 'repository_id' do
+    let(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
+    let(:processing) { FactoryGirl.build(:processing) }
+    let(:repository) { FactoryGirl.build(:repository, id: 2) }
 
-      context 'with valid ModuleResult instance' do
+    context 'with valid ModuleResult instance' do
+      before :each do
+        MetricResult.expects(:find).with(metric_result.id.to_s).returns(metric_result)
+        metric_result.expects(:processing).returns(processing)
+        processing.expects(:repository).returns(repository)
+        post :repository_id, id: metric_result.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:success) }
+
+      it 'should return the repository_id' do
+        expect(JSON.parse(response.body)).to eq(JSON.parse({repository_id: repository.id}.to_json))
+      end
+    end
+
+    context 'with invalid ModuleResult instance' do
+      let(:error_hash) { {error: 'RecordNotFound'} }
+
+      before :each do
+        MetricResult.expects(:find).with(metric_result.id.to_s).raises(ActiveRecord::RecordNotFound)
+
+        post :repository_id, id: metric_result.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:unprocessable_entity) }
+
+      it 'should return the error_hash' do
+        expect(JSON.parse(response.body)).to eq(JSON.parse(error_hash.to_json))
+      end
+    end
+  end
+
+  describe 'metric_configuration' do
+    let!(:metric_result) { FactoryGirl.build(:metric_result, id: 1) }
+
+    context 'with valid ModuleResult instance' do
+      pending 'Active Resource does not support to_json method' do
         let(:metric_configuration) { FactoryGirl.build(:metric_configuration) }
         before :each do
           MetricResult.expects(:find).with(metric_result.id.to_s).returns(metric_result)
@@ -82,21 +82,21 @@ describe MetricResultsController do
           expect(JSON.parse(response.body)).to eq(JSON.parse({metric_configuration: metric_configuration}.to_json))
         end
       end
+    end
 
-      context 'with invalid ModuleResult instance' do
-        let(:error_hash) { {error: 'RecordNotFound'} }
+    context 'with invalid ModuleResult instance' do
+      let(:error_hash) { {error: 'RecordNotFound'} }
 
-        before :each do
-          MetricResult.expects(:find).with(metric_result.id.to_s).raises(ActiveRecord::RecordNotFound)
+      before :each do
+        MetricResult.expects(:find).with(metric_result.id.to_s).raises(ActiveRecord::RecordNotFound)
 
-          get :metric_configuration, id: metric_result.id, format: :json
-        end
+        get :metric_configuration, id: metric_result.id, format: :json
+      end
 
-        it { is_expected.to respond_with(:unprocessable_entity) }
+      it { is_expected.to respond_with(:unprocessable_entity) }
 
-        it 'should return the error_hash' do
-          expect(JSON.parse(response.body)).to eq(JSON.parse(error_hash.to_json))
-        end
+      it 'should return the error_hash' do
+        expect(JSON.parse(response.body)).to eq(JSON.parse(error_hash.to_json))
       end
     end
   end
--- spec/lib/processor/aggregator_spec.rb
+++ spec/lib/processor/aggregator_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 require 'processor'
 
 describe Processor::Aggregator do
-  describe 'methods' do
-    describe 'task' do
-      let(:configuration) { FactoryGirl.build(:configuration) }
-      let!(:code_dir) { "/tmp/test" }
-      let!(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", configuration: configuration, code_directory: code_dir) }
-      let!(:root_module_result) { FactoryGirl.build(:module_result) }
-      let!(:processing) { FactoryGirl.build(:processing, repository: repository, root_module_result: root_module_result) }
-      let!(:child) { FactoryGirl.build(:module_result_class_granularity, parent: root_module_result) }
-      let!(:native_metric_configurations) {
-        [FactoryGirl.build(:metric_configuration, id: 1),
-         FactoryGirl.build(:metric_configuration, metric: FactoryGirl.build(:kalibro_gatekeeper_client_loc), id: 2)]
-      }
-      let!(:compound_metric_configurations) { [FactoryGirl.build(:compound_metric_configuration)] }
-      let!(:native_metrics) { { "Analizo" => native_metric_configurations } }
-      let!(:all_metrics) { [native_metric_configurations.first.metric, native_metric_configurations.last.metric] }
-      let!(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
+  pending 'method == not working for ActiveResource' do
+    describe 'methods' do
+      describe 'task' do
+        let(:configuration) { FactoryGirl.build(:configuration) }
+        let!(:code_dir) { "/tmp/test" }
+        let!(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", configuration: configuration, code_directory: code_dir) }
+        let!(:root_module_result) { FactoryGirl.build(:module_result) }
+        let!(:processing) { FactoryGirl.build(:processing, repository: repository, root_module_result: root_module_result) }
+        let!(:child) { FactoryGirl.build(:module_result_class_granularity, parent: root_module_result) }
+        let!(:native_metric_configurations) {
+          [FactoryGirl.build(:metric_configuration, id: 1),
+           FactoryGirl.build(:metric_configuration, metric: FactoryGirl.build(:kalibro_client_loc), id: 2)]
+        }
+        let!(:compound_metric_configurations) { [FactoryGirl.build(:compound_metric_configuration)] }
+        let!(:native_metrics) { { "Analizo" => native_metric_configurations } }
+        let!(:all_metrics) { [native_metric_configurations.first.metric, native_metric_configurations.last.metric] }
+        let!(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
 
-      before :each do
-        context.native_metrics = native_metrics
-      end
+        before :each do
+          context.native_metrics = native_metrics
+        end
 
-      context 'when the module result tree has been well-built' do
-        pending 'kalibro client integration' do
+        context 'when the module result tree has been well-built' do
 
           before :each do
             root_module_result.expects(:pre_order).returns([root_module_result, child])
@@ -36,11 +36,11 @@ describe Processor::Aggregator do
           end
         end
       end
-    end
 
-    describe 'state' do
-      it 'is expected to return "AGGREGATING"' do
-        expect(Processor::Aggregator.state).to eq("AGGREGATING")
+      describe 'state' do
+        it 'is expected to return "AGGREGATING"' do
+          expect(Processor::Aggregator.state).to eq("AGGREGATING")
+        end
       end
     end
   end
