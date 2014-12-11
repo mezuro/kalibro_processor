b3f2831c47709f06825420b022054339826e313b
Heitor Reis <marcheing@gmail.com>
2014-12-11 14:20:00 -0200

    Tree builder tests already fixed
    Signed off by: Daniel Alves <danpaulalves@gmail.com>

--- spec/lib/processor/tree_builder_spec.rb
+++ spec/lib/processor/tree_builder_spec.rb
@@ -2,42 +2,40 @@ require 'rails_helper'
 require 'processor'
 
 describe Processor::TreeBuilder do
-  pending 'kalibro client integration' do
-    describe 'methods' do
-      let!(:configuration) { FactoryGirl.build(:configuration) }
-      let!(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", configuration: configuration) }
-      let!(:processing) { FactoryGirl.build(:processing, repository: repository) }
-      let!(:root_module_result) { FactoryGirl.build(:module_result) }
-      let!(:module_result) { FactoryGirl.build(:module_result_class_granularity) }
-      let!(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
+  describe 'methods' do
+    let!(:configuration) { FactoryGirl.build(:configuration) }
+    let!(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", configuration: configuration) }
+    let!(:processing) { FactoryGirl.build(:processing, repository: repository) }
+    let!(:root_module_result) { FactoryGirl.build(:module_result) }
+    let!(:module_result) { FactoryGirl.build(:module_result_class_granularity) }
+    let!(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
 
-      describe 'task' do
-        context 'when there are module results' do
-          before :each do
-            filtered_module_results = Object.new
-            module_result_limits = Object.new
-            module_result.expects(:update).with(parent: root_module_result).returns(true)
-            module_result_limits.expects(:offset).with(0).returns([module_result, root_module_result])
-            module_result_limits.expects(:offset).with(100).returns([])
-            filtered_module_results.expects(:limit).at_least_once.with(100).returns(module_result_limits)
-            ModuleResult.expects(:where).with(processing: processing).at_least_once.returns(filtered_module_results)
-            module_result.kalibro_module.expects(:parent).returns(root_module_result.kalibro_module)
-            ModuleResult.expects(:find_by_module_and_processing).at_least_once.returns(nil)
-            root_module_result.kalibro_module.expects(:parent).twice.returns(nil)
-            ModuleResult.expects(:create).with(kalibro_module: root_module_result.kalibro_module, processing: processing).returns(root_module_result)
-            processing.expects(:update).twice.with(root_module_result: root_module_result).returns(true)
-          end
+    describe 'task' do
+      context 'when there are module results' do
+        before :each do
+          filtered_module_results = Object.new
+          module_result_limits = Object.new
+          module_result.expects(:update).with(parent: root_module_result).returns(true)
+          module_result_limits.expects(:offset).with(0).returns([module_result, root_module_result])
+          module_result_limits.expects(:offset).with(100).returns([])
+          filtered_module_results.expects(:limit).at_least_once.with(100).returns(module_result_limits)
+          ModuleResult.expects(:where).with(processing: processing).at_least_once.returns(filtered_module_results)
+          module_result.kalibro_module.expects(:parent).returns(root_module_result.kalibro_module)
+          ModuleResult.expects(:find_by_module_and_processing).at_least_once.returns(nil)
+          root_module_result.kalibro_module.expects(:parent).twice.returns(nil)
+          ModuleResult.expects(:create).with(kalibro_module: root_module_result.kalibro_module, processing: processing).returns(root_module_result)
+          processing.expects(:update).twice.with(root_module_result: root_module_result).returns(true)
+        end
 
-          it 'is expected to build the module results tree' do
-            Processor::TreeBuilder.task(context)
-          end
+        it 'is expected to build the module results tree' do
+          Processor::TreeBuilder.task(context)
         end
       end
+    end
 
-      describe 'state' do
-        it 'is expected to return "BUILDING"' do
-          expect(Processor::TreeBuilder.state).to eq("BUILDING")
-        end
+    describe 'state' do
+      it 'is expected to return "BUILDING"' do
+        expect(Processor::TreeBuilder.state).to eq("BUILDING")
       end
     end
   end
