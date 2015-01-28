55579a3f5a1d1310ada5fde6ba753cfda5ea34d4
Daniel Alves <danpaulalves@gmail.com>
2015-01-28 14:53:29 -0200

    Implemented validations for project
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :repositories
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Project, :type => :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
   describe 'associations' do
     it { is_expected.to have_many(:repositories)}
   end
