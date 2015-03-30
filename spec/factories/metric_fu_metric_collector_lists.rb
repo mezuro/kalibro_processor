24b95b73e62c806832da1c95271fce13fc86a7a9
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-03-30 16:58:56 -0300

    Fixes unit tests for flog parser

--- spec/factories/metric_fu_metric_collector_lists.rb
+++ spec/factories/metric_fu_metric_collector_lists.rb
@@ -5,7 +5,8 @@ end
 FactoryGirl.define do
   factory :metric_fu_metric_collector_lists, class: MetricFuMetricCollectorLists do
     flog_results { {:flog => {:method_containers => [{:path => "app/models/repository.rb",
-                   :methods => {"Repository#process"=>{:operators=>{:perform_later=>1.1}, :score=>1.1, :path=>"app/models/repository.rb:30"}, 
-                                "main#none"=>{:operators=>{:require=>36.20000000000003}, :score=>2.0, :path=>nil}}}]}} }
+                   :methods => {"Repository#process"=>{:operators=>{:perform_later=>1.1}, :score=>1.1, :path=>"app/models/repository.rb:30"},
+                                "Repository#reprocess"=>{:operators=>{:perform_later=>1.1}, :score=>2.0, :path=>"app/models/repository.rb:30"},
+                                "main#none"=>{:operators=>{:require=>36.20000000000003}, :score=>3.0, :path=>nil}}}]}} }
   end
 end
--- spec/lib/metric_collector/native/metric_fu/parser/flog_spec.rb
+++ spec/lib/metric_collector/native/metric_fu/parser/flog_spec.rb
@@ -5,19 +5,19 @@ describe MetricCollector::Native::MetricFu::Parser::Flog do
   describe 'parse' do
     let!(:flog_results) { FactoryGirl.build(:metric_fu_metric_collector_lists).flog_results[:flog] }
     let!(:processing) { FactoryGirl.build(:processing) }
-    let!(:kalibro_module_class) { FactoryGirl.build(:kalibro_module_with_class_granularity) }
-    let!(:kalibro_module_method) { FactoryGirl.build(:kalibro_module_with_method_granularity) }
+    let!(:kalibro_module_method_process) { FactoryGirl.build(:kalibro_module_with_method_granularity) }
+    let!(:kalibro_module_method_reprocess) { FactoryGirl.build(:kalibro_module_with_method_granularity) }
     let!(:module_result) { FactoryGirl.build(:module_result) }
     let!(:metric_configuration) { FactoryGirl.build(:flog_metric_configuration) }
 
     context 'when there are no ModuleResults with the same module and processing' do
       before :each do
-        KalibroModule.expects(:new).with(long_name: "app.models.repository.process", granularity: Granularity::METHOD).returns(kalibro_module_method)
-        KalibroModule.expects(:new).with(long_name: "app.models.repository", granularity: Granularity::CLASS).returns(kalibro_module_class)
-        ModuleResult.expects(:find_by_module_and_processing).with(kalibro_module_method, processing).returns(nil)
-        ModuleResult.expects(:find_by_module_and_processing).with(kalibro_module_class, processing).returns(module_result)
-        kalibro_module_method.expects(:save)
-        ModuleResult.expects(:create).with(kalibro_module: kalibro_module_method, processing: processing).returns(module_result)
+        KalibroModule.expects(:new).with(long_name: "app.models.repository.process", granularity: Granularity::METHOD).returns(kalibro_module_method_process)
+        KalibroModule.expects(:new).with(long_name: "app.models.repository.reprocess", granularity: Granularity::METHOD).returns(kalibro_module_method_reprocess)
+        ModuleResult.expects(:find_by_module_and_processing).with(kalibro_module_method_process, processing).returns(nil)
+        ModuleResult.expects(:find_by_module_and_processing).with(kalibro_module_method_reprocess, processing).returns(module_result)
+        kalibro_module_method_process.expects(:save)
+        ModuleResult.expects(:create).with(kalibro_module: kalibro_module_method_process, processing: processing).returns(module_result)
         MetricResult.expects(:create).with(metric: metric_configuration.metric, value: 1.1, module_result: module_result, metric_configuration_id: metric_configuration.id)
         MetricResult.expects(:create).with(metric: metric_configuration.metric, value: 2.0, module_result: module_result, metric_configuration_id: metric_configuration.id)
       end
