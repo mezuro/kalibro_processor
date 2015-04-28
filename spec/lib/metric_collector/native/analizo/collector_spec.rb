88fc2057c90137753fbc1c6bde1a58c2faa2f8b1
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-04-28 12:54:49 -0300

    Completed the unit tests for Analizo collector

--- spec/lib/metric_collector/native/analizo/collector_spec.rb
+++ spec/lib/metric_collector/native/analizo/collector_spec.rb
@@ -33,11 +33,32 @@ describe MetricCollector::Native::Analizo::Collector, :type => :model do
       let(:wanted_metrics_list) { [FactoryGirl.build(:metric_configuration)] }
       let(:processing) { FactoryGirl.build(:processing) }
 
+      before :each do
+        MetricCollector::Native::Analizo::Collector.any_instance.expects(:parse_supported_metrics).returns(analizo_metric_collector_list.parsed)
+      end
+
       it 'is expected to call the Runner and Parser' do
         MetricCollector::Native::Analizo::Runner.any_instance.expects(:run).returns(analizo_metric_collector_list.parsed_result)
         MetricCollector::Native::Analizo::Parser.any_instance.expects(:parse_all).with(analizo_metric_collector_list.parsed_result)
 
         subject.collect_metrics(absolute_path, wanted_metrics_list, processing)
+      end
+    end
+
+    describe 'parse_supported_metrics' do
+      context 'when Analizo is available' do
+        it 'is expected to call the Analizo metric listing and parse it into NativeMatrics' do
+          MetricCollector::Native::Analizo::Collector.any_instance.expects(:`).twice.with("analizo metrics --list").returns(analizo_metric_collector_list.raw)
+
+          expect(subject.parse_supported_metrics["acc"]).to_not be_nil
+        end
+      end
+
+      context 'when Analizo is not available' do
+        it 'is expected to raise a NotFoundError' do
+          MetricCollector::Native::Analizo::Collector.any_instance.expects(:`).with("analizo metrics --list").returns(nil)
+          expect { subject.parse_supported_metrics }.to raise_error(Errors::NotFoundError)
+        end
       end
     end
   end
