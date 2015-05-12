47d5497c95b8ad5a93f1d532817f9cbdb1969068
Heitor Reis <marcheing@gmail.com>
2015-05-12 11:31:00 -0300

    Refactoring static methods from parser
    These methods are probably going to be used on other parsers
    
    Signed-off-by: Pedro Scocco <pedroscocco@gmail.com>

--- lib/metric_collector/native/metric_fu/parser/flog.rb
+++ lib/metric_collector/native/metric_fu/parser/flog.rb
@@ -20,31 +20,6 @@ module MetricCollector
               end
             end
           end
-
-          private
-
-          def self.module_name_prefix(file_name)
-            # Generates a module name by removing the file extension, replacing slashes with dots, and internal dots with underscores
-            without_extension = file_name.rpartition('.').first
-            without_extension.gsub('.', '_').gsub('/', '.')
-          end
-
-          def self.module_name_suffix(module_name)
-            return nil if module_name.end_with?("#none")
-            return "." + module_name.gsub(/#|::/, ".")
-          end
-
-          def self.module_result(module_name, granularity, processing)
-            kalibro_module = KalibroModule.new({long_name: module_name, granularity: granularity})
-            module_result = ModuleResult.find_by_module_and_processing(kalibro_module, processing)
-
-            if module_result.nil?
-              kalibro_module.save
-              ModuleResult.create(kalibro_module: kalibro_module, processing: processing)
-            else
-              module_result
-            end
-          end
         end
       end
     end
--- lib/metric_collector/native/metric_fu/parser/interface.rb
+++ lib/metric_collector/native/metric_fu/parser/interface.rb
@@ -4,6 +4,32 @@ module MetricCollector
       module Parser
         class Interface
           def parse(collected_metrics_hash); raise NotImplementedError; end
+
+          private
+
+          def self.module_name_prefix(file_name)
+            # Generates a module name by removing the file extension, replacing slashes with dots, and internal dots with underscores
+            without_extension = file_name.rpartition('.').first
+            without_extension.gsub('.', '_').gsub('/', '.')
+          end
+
+          def self.module_name_suffix(module_name)
+            return nil if module_name.end_with?("#none")
+            # Removing the self part of a static method to conform to flog's standard
+            return "." + module_name.gsub(/self\./, "").gsub(/#|::/, ".")
+          end
+
+          def self.module_result(module_name, granularity, processing)
+            kalibro_module = KalibroModule.new({long_name: module_name, granularity: granularity})
+            module_result = ModuleResult.find_by_module_and_processing(kalibro_module, processing)
+
+            if module_result.nil?
+              kalibro_module.save
+              ModuleResult.create(kalibro_module: kalibro_module, processing: processing)
+            else
+              module_result
+            end
+          end
         end
       end
     end
