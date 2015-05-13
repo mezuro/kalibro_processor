377d9fc06ff1fb1533c240cd9474456dcc9bf153
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-05-13 19:24:03 -0300

    MetricResultsChecker
    It should create missing MetricResults
    
    Signed off by: Pedro Scocco <pedroscocco@gmail.com>

---
+++ lib/processor/metric_results_checker.rb
@@ -0,0 +1,36 @@
+module Processor
+  class MetricResultsChecker < ProcessingStep
+    protected
+
+    def self.task(context)
+      wanted_metrics = {}
+
+      context.native_metrics.each do |metric_collector_name, metric_configurations|
+        metric_configurations.each { |metric_configuration| wanted_metrics[metric_configuration.metric] = metric_configuration }
+      end
+
+      context.processing.module_results.each do |module_result|
+        metrics_check_list = wanted_metrics.clone
+
+        module_result.metric_results.each { |metric_result| metrics_check_list.delete(metric_result.metric) }
+
+        metrics_check_list.each do |metric, metric_configuration|
+          MetricResult.create(metric: metric,
+                              value: default_value_from(metric),
+                              module_result: module_result,
+                              metric_configuration_id: metric_configuration.id)
+        end
+      end
+    end
+
+    def self.state
+      "CHECKING"
+    end
+
+    private
+
+    def self.default_value_from(metric)
+      "MetricCollector::Native::#{metric.metric_collector_name}::Parser".constantize.default_value_from(metric.code)
+    end
+  end
+end
---
+++ spec/lib/processor/metric_results_checker_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require 'metric_collector'
+require 'processor'
+
+describe Processor::MetricResultsChecker do
+  describe 'methods' do
+    describe 'task' do
+      let(:kalibro_configuration) { FactoryGirl.build(:kalibro_configuration) }
+      let(:code_dir) { "/tmp/test" }
+      let(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", kalibro_configuration: kalibro_configuration, code_directory: code_dir) }
+      let(:flog_metric_configuration) { FactoryGirl.build(:flog_metric_configuration) }
+      let(:saikuro_metric_configuration) { FactoryGirl.build(:saikuro_metric_configuration, id: 1) }
+      let(:metric_result) { FactoryGirl.build(:metric_result,
+                                               metric_configuration: saikuro_metric_configuration) }
+      let(:module_result) { FactoryGirl.build(:module_result_class_granularity) }
+      let(:processing) { FactoryGirl.build(:processing, repository: repository, root_module_result: module_result, module_results: [module_result]) }
+      let(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
+
+      context 'when there is a wanted metric that produced no results for the given module' do
+        let!(:default_value) { 0.0 }
+        before :each do
+          context.native_metrics['MetricFu'] << flog_metric_configuration
+          context.native_metrics['MetricFu'] << saikuro_metric_configuration
+          metric_result.expects(:metric).returns(saikuro_metric_configuration.metric)
+          module_result.stubs(:metric_results).returns([metric_result])
+          MetricCollector::Native::MetricFu::Parser.expects(:default_value_from).with(flog_metric_configuration.metric.code).returns(default_value)
+        end
+
+        it 'is expected to create a MetricResult with the default value for this metric' do
+          MetricResult.expects(:create).with(metric: flog_metric_configuration.metric, value: default_value, module_result: module_result, metric_configuration_id: flog_metric_configuration.id)
+
+          Processor::MetricResultsChecker.task(context)
+        end
+      end
+
+    end
+
+    describe 'state' do
+      it 'is expected to return CHECKING' do
+        expect(Processor::MetricResultsChecker.state).to eq('CHECKING')
+      end
+    end
+  end
+end
