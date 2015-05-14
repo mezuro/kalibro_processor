ff09421318a95ebcd95e7930adbaeb54cabef7de
Daniel Miranda <danielkza2@gmail.com>
2015-05-14 17:44:48 -0300

    Fix metric checker adding defaults even if results existed

--- lib/processor/metric_results_checker.rb
+++ lib/processor/metric_results_checker.rb
@@ -2,21 +2,28 @@ module Processor
   class MetricResultsChecker < ProcessingStep
     protected
 
+    def metric_key(metric)
+      metric.metric_collector_name.to_s + "#" + metric.code.to_s + "#" + metric.scope.to_s
+    end
+
     def self.task(context)
       wanted_metrics = {}
 
       context.native_metrics.each do |metric_collector_name, metric_configurations|
-        metric_configurations.each { |metric_configuration| wanted_metrics[metric_configuration.metric] = metric_configuration }
+        metric_configurations.each { |metric_configuration|
+          wanted_metrics[metric_key(metric_configuration.metric)] = metric_configuration
+        }
       end
 
       context.processing.module_results.each do |module_result|
         metrics_check_list = wanted_metrics.clone
 
-        module_result.metric_results.each { |metric_result| metrics_check_list.delete(metric_result.metric) }
+        module_result.metric_results.each { |metric_result|
+          metrics_check_list.delete(metric_key(metric_result.metric))
+        }
 
         metrics_check_list.each do |metric, metric_configuration|
-          MetricResult.create(metric: metric,
-                              value: default_value_from(metric),
+          MetricResult.create(value: default_value_from(metric),
                               module_result: module_result,
                               metric_configuration_id: metric_configuration.id)
         end
