4551c438305c99e3d5048778ec397fd13cb88caa
Parley Martins <parleypachecomartins@gmail.com>
2015-05-28 15:35:31 -0300

    Make runner more DRY

--- lib/metric_collector/native/radon/runner.rb
+++ lib/metric_collector/native/radon/runner.rb
@@ -2,36 +2,47 @@ module MetricCollector
   module Native
     module Radon
       class Runner
-        def initialize(attributes={repository_path: nil})
-          @repository_path = attributes[:repository_path]
+
+        def initialize(attributes={repository_path: nil, chosen_metrics:["raw", "cc", "mi"]})
+          @repository_path = repository_path
+          @chosen_metrics = chosen_metrics
+
         end
 
         def run
-          run_cyclomatic_complexity
-          run_maintainability
-          run_raw
+          for metric in chosen_metrics do
+            collect_metric(metric)
+          end
         end
 
         def clean_output
-          File.delete('radon_cc_output.json') if File.exists?('radon_cc_output.json')
-          File.delete('radon_mi_output.json') if File.exists?('radon_mi_output.json')
-          File.delete('radon_raw_output.json') if File.exists?('radon_raw_output.json')
+          for metric in @output_paths do
+            File.delete(@output_paths[metric]) if File.exists?(@output_paths[metric])
+          end
+        end
+
+        def chosen_metrics
+          @chosen_metrics
+        end
+
+        def generate_output_paths
+          @output_paths = {}
+          for metric in chosen_metrics do
+            file_path = @repository_path
+
+            while File.exists?(file_path)
+              @output_paths[metric] = "#{@repository_path}/#{Digest::MD5.hexdigest(Time.now.to_s)}.json"
+            end
+          end
+          @output_paths
+
         end
 
         private
 
-        def run_cyclomatic_complexity
-          Dir.chdir(@repository_path) { `radon cc -s --json . > radon_cc_output.json` }
+        def collect_metric(metric)
+          Dir.chdir(@repository_path) { `radon #{metric} -s --json . > #{@output_paths[metric]}` }
         end
-
-        def run_maintainability
-          Dir.chdir(@repository_path) { `radon mi -s --json . > radon_mi_output.json` }
-        end
-
-        def run_raw
-          Dir.chdir(@repository_path) { `radon raw -s --json . > radon_raw_output.json` }
-        end
-
       end
     end
   end
