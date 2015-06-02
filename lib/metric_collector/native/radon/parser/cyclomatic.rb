65b0f39525b61b33776214deed05784f07b96c65
LucasLeite <lucas.srl08@gmail.com>
2015-06-02 12:56:13 -0300

    Implementing parse for cyclomatic complexity

--- lib/metric_collector/native/radon/parser/cyclomatic.rb
+++ lib/metric_collector/native/radon/parser/cyclomatic.rb
@@ -3,20 +3,19 @@ module MetricCollector
     module Radon
       module Parser
         class Cyclomatic < MetricCollector::Native::Radon::Parser::Base
-
-          def self.parse(output_path)
-            #puts "output_path = #{output_path}"
+          def self.parse(output_path, processing = nil, metric_configuration = nil)
             output_file = File.read("#{output_path}/radon_cc_output.json")
     		    data_hash = JSON.parse(output_file)
 
     		    data_hash.each do |file_name, result_hash|
               name_prefix = module_name_prefix(file_name)
               result_hash.each do |key|
-                name_suffix = module_name_suffix()
+                name_suffix = module_name_suffix(key['name'])
                 value = key['complexity']
                 module_name = name_prefix + name_suffix
-                module_result = module_result(module_name, Granularity::METHOD, processing)
-                #MetricResult.create(metric: metric_configuration.metric, value: value.to_f, module_result: module_result, metric_configuration_id: metric_configuration.id)
+                granularity = Granularity::METHOD
+                module_result = module_result(module_name, granularity, processing)
+                MetricResult.create(metric: metric_configuration.metric, value: value.to_f, module_result: module_result, metric_configuration_id: metric_configuration.id)
               end
             end
             0
--- lib/metric_collector/native/radon/runner.rb
+++ lib/metric_collector/native/radon/runner.rb
@@ -5,47 +5,40 @@ module MetricCollector
         def initialize(attributes={repository_path: nil, chosen_metrics:["raw", "cc", "mi"]})
           @repository_path = repository_path
           @chosen_metrics = chosen_metrics
-
         end
 
         def repository_path
           @repository_path
+        end
 
         end
        
         def run
-          for metric in chosen_metrics do
-            collect_metric(metric)
-          end
+          run_cyclomatic_complexity
+          run_maintainability
+          run_raw
         end
 
         def clean_output
-          for metric in @output_paths do
-            File.delete(@output_paths[metric]) if File.exists?(@output_paths[metric])
-          end
-        end
-
-        def chosen_metrics
-          @chosen_metrics
-        end
-
-        def generate_output_paths
-          @output_paths = {}
-          for metric in chosen_metrics do
-            file_path = @repository_path
-
-            while File.exists?(file_path)
-              @output_paths[metric] = "#{@repository_path}/#{Digest::MD5.hexdigest(Time.now.to_s)}.json"
-            end
-          end
-          @output_paths
+          File.delete('radon_cc_output.json') if File.exists?('radon_cc_output.json')
+          File.delete('radon_mi_output.json') if File.exists?('radon_mi_output.json')
+          File.delete('radon_raw_output.json') if File.exists?('radon_raw_output.json')
         end
 
         private
 
-        def collect_metric(metric)
-          Dir.chdir(@repository_path) { `radon #{metric} -s --json . > #{@output_paths[metric]}` }
+        def run_cyclomatic_complexity
+          Dir.chdir(@repository_path) { `radon cc -s --json . > radon_cc_output.json` }
         end
+
+        def run_maintainability
+          Dir.chdir(@repository_path) { `radon mi -s --json . > radon_mi_output.json` }
+        end
+
+        def run_raw
+          Dir.chdir(@repository_path) { `radon raw -s --json . > radon_raw_output.json` }
+        end
+
       end
     end
   end
