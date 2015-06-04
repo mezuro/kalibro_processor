f0a9f31b11157fe476d2cbe3012654e9d93067e5
MaxAlmeida <llewxam100@hotmail.com>
2015-06-04 17:24:15 -0300

    Adding parser for raw metrics

--- lib/metric_collector/native/radon/parser/raw.rb
+++ lib/metric_collector/native/radon/parser/raw.rb
@@ -3,6 +3,26 @@ module MetricCollector
     module Radon
       module Parser
         class Raw < MetricCollector::Native::Radon::Parser::Base
+
+        	def self.parse(output_path, processing = nil, metric_configuration = nil)
+            output_file = File.read("#{output_path}/radon_raw_output.json")
+            data_hash = JSON.parse(output_file)
+
+            data_hash.each do |file_name, result_hash|
+                file_name = module_name_prefix(file_name)
+                value =  result_hash[metric_configuration.metric.code]
+                module_name = file_name
+                granularity = Granularity::METHOD
+                module_result = module_result(module_name, granularity, processing)
+                MetricResult.create(metric: metric_configuration.metric, value: value.to_f, module_result: module_result, metric_configuration_id: metric_configuration.id)
+              end
+
+          end
+
+          def self.default_value
+            0.0
+          end
+
         end
       end
     end
