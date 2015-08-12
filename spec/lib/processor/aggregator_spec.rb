33ebb9f80e0628e8a0e922d05e1f8230712e1152
Pedro Scocco <pedroscocco@gmail.com>
2015-08-12 14:57:27 -0300

    Fixed agregation for same granularity modules
    With Radon we discovered that we can have multi-level package hierarchy
    modules.
    
    Signed off by: Rafael Reggiani Manzo <rr.manzo@gmail.com>

--- spec/lib/processor/aggregator_spec.rb
+++ spec/lib/processor/aggregator_spec.rb
@@ -9,29 +9,56 @@ describe Processor::Aggregator do
       let!(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", kalibro_configuration: kalibro_configuration, code_directory: code_dir) }
       let!(:root_module_result) { FactoryGirl.build(:module_result) }
       let!(:processing) { FactoryGirl.build(:processing, repository: repository, root_module_result: root_module_result) }
-      let!(:child) { FactoryGirl.build(:module_result_class_granularity, parent: root_module_result) }
-      let!(:native_metric_configurations) {
-        [FactoryGirl.build(:metric_configuration, id: 1),
-         FactoryGirl.build(:metric_configuration, metric: FactoryGirl.build(:loc), id: 2)]
-      }
       let!(:compound_metric_configurations) { [FactoryGirl.build(:compound_metric_configuration)] }
-      let!(:native_metrics) { { "Analizo" => native_metric_configurations } }
-      let!(:all_metrics) { [native_metric_configurations.first.metric, native_metric_configurations.last.metric] }
       let!(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
 
-      before :each do
-        context.native_metrics = native_metrics
-      end
-
       context 'when the module result tree has been well-built' do
+        context 'with a linear hierarchy' do
+          let!(:child) { FactoryGirl.build(:module_result_class_granularity, parent: root_module_result) }
+          let!(:native_metric_configurations) {
+            [FactoryGirl.build(:metric_configuration, id: 1),
+             FactoryGirl.build(:metric_configuration, metric: FactoryGirl.build(:loc), id: 2)]
+          }
+          let!(:native_metrics) { { "Analizo" => native_metric_configurations } }
+          let!(:all_metrics) { [native_metric_configurations.first.metric, native_metric_configurations.last.metric] }
+          let!(:metric_results) { [FactoryGirl.build(:metric_result, :with_value, metric_configuration: native_metric_configurations.first, metric: native_metric_configurations.first.metric),
+                                   FactoryGirl.build(:metric_result, :with_value, metric_configuration: native_metric_configurations.last, metric: native_metric_configurations.last.metric)] }
 
-        before :each do
-          root_module_result.expects(:pre_order).returns([root_module_result, child])
-          MetricResult.any_instance.expects(:save)
+          before :each do
+            context.native_metrics = native_metrics
+            child.expects(:metric_results).returns(metric_results)
+            root_module_result.expects(:pre_order).returns([root_module_result, child])
+            MetricResult.any_instance.expects(:save)
+          end
+
+          it 'is expected to aggregate results' do
+            Processor::Aggregator.task(context)
+          end
         end
 
-        it 'is expected to aggregate results' do
-          Processor::Aggregator.task(context)
+        context 'with a multi-level package hierarchy' do
+          let!(:parent) { FactoryGirl.build(:module_result, :package, parent: root_module_result) }
+          let!(:child) { FactoryGirl.build(:module_result, :package, parent: parent) }
+          let!(:another_child) { FactoryGirl.build(:module_result, :package, parent: parent) }
+          let!(:native_metric_configurations) {
+            [FactoryGirl.build(:metric_configuration, metric: FactoryGirl.build(:maintainability_metric), id: 1)]
+          }
+          let!(:native_metrics) { { "Radon" => native_metric_configurations } }
+          let!(:all_metrics) { [native_metric_configurations.first.metric] }
+          let!(:metric_results) { [FactoryGirl.build(:metric_result, :with_value, metric_configuration: native_metric_configurations.first, metric: native_metric_configurations.first.metric)] }
+
+          before :each do
+            context.native_metrics = native_metrics
+            child.expects(:metric_results).returns(metric_results)
+            another_child.expects(:metric_results).returns(metric_results)
+            root_module_result.expects(:pre_order).returns([root_module_result, parent, child, another_child])
+            MetricResult.any_instance.expects(:save).twice
+            MetricResultAggregator.expects(:aggregated_value).twice
+          end
+
+          it 'is expected to aggregate results' do
+            Processor::Aggregator.task(context)
+          end
         end
       end
     end
