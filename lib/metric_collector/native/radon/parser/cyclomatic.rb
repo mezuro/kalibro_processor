5c5e07f7608b8ae1e2dd1047b7cb24429ccae3f5
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-08-24 16:40:17 -0300

    Refactored and commented Radon::Parser::Cyclomatic
    It is kind of complex so some comments were needed.
    
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- lib/metric_collector/native/radon/parser/cyclomatic.rb
+++ lib/metric_collector/native/radon/parser/cyclomatic.rb
@@ -12,7 +12,7 @@ module MetricCollector
           end
 
           def self.parse(collected_metrics_hash, processing, metric_configuration)
-            self.results_enumerator(collected_metrics_hash).each do |module_name, result, granularity|
+            self.pre_parse(collected_metrics_hash).each do |module_name, result, granularity|
               module_name = module_name + '.' + result['name']
               module_result = module_result(module_name, granularity, processing)
               value = result['complexity'].to_f
@@ -23,16 +23,12 @@ module MetricCollector
 
           private
 
-          def self.results_enumerator(collected_metrics_hash)
-            Enumerator.new do |y|
+          # This method checks for errors found by Radon, switch the valid results between METHOD and FUNCTION
+          # Returning a Enumerator instance (a triples list) with [module_name, result, granularity]
+          def self.pre_parse(collected_metrics_hash)
+            Enumerator.new do |parsed_result|
               collected_metrics_hash.each do |file_name, results|
-                unless results.is_a?(Array)
-                  error = results['error'] if results.is_a?(Hash)
-                  error ||= 'Unknown error'
-                  Rails.logger.debug("Radon: error parsing file #{file_name}: #{error}")
-
-                  next
-                end
+                next if has_error?(file_name, results) # If Radon couldn't parse the file we don't want to continue since the output is a error message
 
                 file_module_name = module_name_prefix(file_name)
 
@@ -42,17 +38,27 @@ module MetricCollector
                     class_name = result['name']
 
                     result['methods'].each do |method|
-                      y << ["#{file_module_name}.#{class_name}", method, granularity]
+                      parsed_result << ["#{file_module_name}.#{class_name}", method, granularity]
                     end
                   else
-                    if result['type'] == 'method'
-                      next
-                    end
+                    next if result['type'] == 'method' # These entries are duplicated on Radon's output
                     granularity = KalibroClient::Entities::Miscellaneous::Granularity::FUNCTION
-                    y << [file_module_name, result, granularity]
+                    parsed_result << [file_module_name, result, granularity]
                   end
                 end
               end
+            end
+          end
+
+          def self.has_error?(file_name, results)
+            unless results.is_a?(Array)
+              error = results['error'] if results.is_a?(Hash)
+              error ||= 'Unknown error'
+              Rails.logger.debug("Radon: error parsing file #{file_name}: #{error}") # This debug print has been kept in order that if there is some non-fatal error it does not get ignored
+
+              true
+            else
+              false
             end
           end
         end
