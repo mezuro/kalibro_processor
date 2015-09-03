de6d2d341389bb45bbf78151b4466e631cda6b77
Daniel Miranda <danielkza2@gmail.com>
2015-09-03 15:49:23 -0300

    Fixed MetricResult renaming migrations again
    Since we were renaming classes, using them in the migration would not
    work if the tree was in a newer commit. Unfortunately the only sane way
    to fix that is to revert to raw SQL. At least do it in a pretty way that
    allows rollbacks.

--- db/migrate/20150903132734_rename_hotspot_result_to_hotspot_metric_result.rb
+++ db/migrate/20150903132734_rename_hotspot_result_to_hotspot_metric_result.rb
@@ -1,6 +1,17 @@
 class RenameHotspotResultToHotspotMetricResult < ActiveRecord::Migration
   def change
-    BaseMetricResult.where(type: 'HotspotResult').update_all(type: 'HotspotMetricResult')
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE metric_results SET type = 'HotspotMetricResult' WHERE type = 'HotspotResult'
+        SQL
+      end
+      dir.down do
+        execute <<-SQL
+          UPDATE metric_results SET type = 'HotspotResult' WHERE type = 'HotspotMetricResult'
+        SQL
+      end
+    end
     rename_column :metric_results, :related_hotspot_results_id, :related_hotspot_metric_results_id
   end
 end
--- db/migrate/20150903142536_rename_metric_result_to_tree_metric_result.rb
+++ db/migrate/20150903142536_rename_metric_result_to_tree_metric_result.rb
@@ -1,5 +1,16 @@
 class RenameMetricResultToTreeMetricResult < ActiveRecord::Migration
   def change
-    BaseMetricResult.where(type: 'MetricResult').update_all(type: 'TreeMetricResult')
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE metric_results SET type = 'TreeMetricResult' WHERE type = 'MetricResult'
+        SQL
+      end
+      dir.down do
+        execute <<-SQL
+          UPDATE metric_results SET type = 'MetricResult' WHERE type = 'TreeMetricResult'
+        SQL
+      end
+    end
   end
 end
