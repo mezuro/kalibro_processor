fa7b986ed35af9883308fd5cda2352e2346560b4
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-10-09 15:22:15 -0300

    Add feature for MetricResult#module_result
    This is a future method which is a requirement for Prezento's Hotspot
    MetricResult listing.

---
+++ features/metric_result/module_result.feature
@@ -0,0 +1,20 @@
+Feature: ModuleResult retrieval
+  In order to retrive the module result associated to a given MetricResult
+  As a kalibro_client developer
+  I should be able to make a request that returns the ModuleResult of a given MetricResult id
+
+  @clear_repository @kalibro_configuration_restart @wip
+  Scenario: With a valid MetricResult id
+    Given I have a sample configuration with the Flay hotspot metric
+    And I have the kalibro processor ruby repository with revision "v0.11.0"
+    And I have a processing within the sample repository
+    And I run for the given repository
+    And I have the root ModuleResult of the given processing
+    And I have the first MetricResult of the given ModuleResult
+    When I request for the ModuleResult associated with the given MetricResult's id
+    Then I should get the given ModuleResult
+
+  @wip
+  Scenario: With an invalid MetricResult id
+    When I request for the ModuleResult of the MetricResult with id "42"
+    Then I should get an error response
--- features/step_definitions/module_result_steps.rb
+++ features/step_definitions/module_result_steps.rb
@@ -1,3 +1,12 @@
+Given(/^I have the root ModuleResult of the given processing$/) do
+  @processing.reload # The processing instance in memory might not be synced with the database modifications made by a previous processing
+  @module_result = @processing.root_module_result
+end
+
+Given(/^I have the first MetricResult of the given ModuleResult$/) do
+  @metric_result = @module_result.metric_results.first
+end
+
 When(/^I request the hotspot metric results for the "(.*?)" module result$/) do |module_name|
   @processing.reload
 
@@ -13,6 +22,14 @@ When(/^I request the hotspot metric results for the "(.*?)" module result$/) do 
     .where(metric_configuration_id: @metric_configuration.id)
 end
 
+When(/^I request for the ModuleResult associated with the given MetricResult's id$/) do
+  visit(metric_result_module_result_path(@metric_result.id))
+end
+
+When(/^I request for the ModuleResult of the MetricResult with id "(.*?)"$/) do |id|
+  visit(metric_result_module_result_path(id))
+end
+
 Then(/^I should get the following hotspot metric results:$/) do |table|
   table.hashes.each do |row|
     module_name = row['module name']
@@ -25,4 +42,12 @@ Then(/^I should get the following hotspot metric results:$/) do |table|
     expect(hotspot_metric_result).to be_a(HotspotMetricResult),
       "expected hotspot metric result with module name '#{module_name}' and line #{line_number}, not found"
   end
+end
+
+Then(/^I should get the given ModuleResult json$/) do
+  expect(page.body).to eq(@module_result.to_j)
+end
+
+Then(/^I should get an error response$/) do
+  pending # express the regexp above with the code you wish you had
 end
