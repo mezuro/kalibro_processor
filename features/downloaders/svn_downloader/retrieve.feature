9d3f07e6483c00f982a2ab53243636c954054f73
Eduardo Silva Araújo <duduktamg@hotmail.com>
2015-11-27 16:53:34 -0200

    Handle repository URL change in SVN downloader
    Signed-off-by: Daniel Miranda <danielkza2@gmail.com>

--- features/downloaders/svn_downloader/retrieve.feature
+++ features/downloaders/svn_downloader/retrieve.feature
@@ -4,7 +4,14 @@ Feature: SvnDownloader retrieve
   I should be able to retrieve them
 
   Scenario: cloning and updating
-  When I call retrieve! from svn with "svn://svn.code.sf.net/p/qt-calculator/code/trunk" and "/tmp/test"
-  Then "/tmp/test" should be a svn repository
-  When I call retrieve! from svn with "svn://svn.code.sf.net/p/qt-calculator/code/trunk" and "/tmp/test"
-  Then "/tmp/test" should be a svn repository
+    When I call retrieve! from svn with "svn://svn.code.sf.net/p/qt-calculator/code/trunk" and "/tmp/test"
+    Then "/tmp/test" should be a svn repository
+    When I call retrieve! from svn with "svn://svn.code.sf.net/p/qt-calculator/code/trunk" and "/tmp/test"
+    Then "/tmp/test" should be a svn repository
+
+  @clear_test_dir
+  Scenario: updating after changing URL
+    When I call retrieve! from svn with "svn://svn.code.sf.net/p/qt-calculator/code/trunk" and "/tmp/test"
+    Then "/tmp/test" should be a svn repository
+    When I call retrieve! from svn with "http://svn.code.sf.net/p/qt-calculator/code/trunk" and "/tmp/test"
+    Then "/tmp/test" should be a svn repository with "http://svn.code.sf.net/p/qt-calculator/code/trunk" as it's remote URL
--- spec/lib/downloaders/svn_downloader_spec.rb
+++ spec/lib/downloaders/svn_downloader_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 require 'downloaders'
 
 describe Downloaders::SvnDownloader do
+  let(:factories) { YAML.load_file('spec/factories/svn_downloader.yml') }
+
   describe 'method' do
     describe 'available?' do
       context 'with svn installed' do
-        let!(:svn_version) { YAML.load_file('spec/factories/svn_downloader.yml')["svn_version"] }
+        let!(:svn_version) { factories["svn_version"] }
         before :each do
           subject.class.expects(:`).with("svn --version").returns(svn_version)
         end
@@ -26,21 +28,40 @@ describe Downloaders::SvnDownloader do
       end
     end
 
+    describe 'get_repository_url' do
+      let(:directory) { "/tmp/qt-calculator-code" }
+      let(:address) { "svn://svn.code.sf.net/p/qt-calculator/code/trunk" }
+      let(:svn_info_command) { "svn info #{directory}" }
+      let(:svn_info_result) { factories["svn_info"] }
+
+      it 'should parse the svn information correctly and return the address' do
+        subject.class.expects(:`).with(svn_info_command).returns(svn_info_result)
+
+        expect(subject.class.get_repository_url(directory)).to eq(address)
+      end
+
+      it 'should raise an error if the command fails' do
+        subject.class.expects(:`).with(svn_info_command).returns(nil)
+
+        expect{ subject.class.get_repository_url(directory) }.to raise_error(Errors::SvnExecuteError)
+      end
+    end
+
     describe 'retrieve! (get)' do
-      let(:directory) { "/tmp/test" }
-      let(:address) { "http://test.test" }
+      let(:directory) { "/tmp/qt-calculator-code" }
+      let(:address) { "http://svn.code.sf.net/p/qt-calculator/code" }
+      let(:other_address) { "svn://svn.code.sf.net/p/qt-calculator/code" }
+      let!(:svn_checkout_result) { factories["svn_checkout"] }
+      let!(:svn_checkout_command) { "svn checkout #{address} #{directory}" }
 
       context "when the directory doesn't exist" do
-        let!(:svn_checkout) { YAML.load_file('spec/factories/svn_downloader.yml')["svn_checkout"] }
-        let!(:command) { "svn checkout #{address} #{directory}" }
-
         before :each do
             Dir.expects(:exist?).with(directory).at_least_once.returns(false)
         end
 
         context 'checking out successfully' do
           it 'is expected to checkout the repository' do
-            subject.class.expects(:`).with(command).returns(svn_checkout)
+            subject.class.expects(:`).with(svn_checkout_command).returns(svn_checkout_result)
 
             subject.class.retrieve!(address, directory, nil)
           end
@@ -48,7 +69,7 @@ describe Downloaders::SvnDownloader do
 
         context 'failling to checkout' do
           it 'should raise an error' do
-            subject.class.expects(:`).with(command).returns(nil)
+            subject.class.expects(:`).with(svn_checkout_command).returns(nil)
             expect {subject.class.get(address, directory, nil)}.to raise_error(Errors::SvnExecuteError)
           end
         end
@@ -59,40 +80,58 @@ describe Downloaders::SvnDownloader do
           let!(:svn_revert_command) { "svn revert -R #{directory}" }
           let!(:svn_update_command) { "svn update #{directory}" }
 
-          context "updating sucessfully" do
+          context "and the URL is the same" do
             before :each do
-               Dir.expects(:exist?).with(directory).returns(true)
-               Dir.expects(:exists?).with("#{directory}/.svn").returns(true)
-               subject.class.expects(:`).with(svn_revert_command).returns("")
-               subject.class.expects(:`).with(svn_update_command).returns("")
+              subject.class.expects(:get_repository_url).with(directory).returns(address)
             end
 
-            it 'should revert the directory changes and update it to the last version (reset)' do
-               expect(subject.class.get(address, directory, nil)).to eq(nil)
+            context "updating sucessfully" do
+              before :each do
+                 Dir.expects(:exist?).with(directory).returns(true)
+                 Dir.expects(:exists?).with("#{directory}/.svn").returns(true)
+                 subject.class.expects(:`).with(svn_revert_command).returns("")
+                 subject.class.expects(:`).with(svn_update_command).returns("")
+              end
+
+              it 'should revert the directory changes and update it to the last version (reset)' do
+                 expect(subject.class.get(address, directory, nil)).to eq(nil)
+              end
+            end
+
+            context "failing to update" do
+              before :each do
+                 Dir.expects(:exist?).with(directory).returns(true)
+                 Dir.expects(:exists?).with("#{directory}/.svn").returns(true)
+                 subject.class.expects(:`).with(svn_revert_command).returns(nil)
+                 subject.class.expects(:`).with(svn_update_command).returns(nil)
+              end
+
+              it 'it should raise an error' do
+                 expect {subject.class.get(address, directory, nil)}.to raise_error(Errors::SvnExecuteError)
+              end
             end
           end
 
-          context "failing to update" do
+          context "and the URL has changed" do
             before :each do
-               Dir.expects(:exist?).with(directory).returns(true)
-               Dir.expects(:exists?).with("#{directory}/.svn").returns(true)
-               subject.class.expects(:`).with(svn_revert_command).returns(nil)
-               subject.class.expects(:`).with(svn_update_command).returns(nil)
+              subject.class.expects(:get_repository_url).with(directory).returns(other_address)
             end
 
-            it 'it should raise an error' do
-               expect {subject.class.get(address, directory, nil)}.to raise_error(Errors::SvnExecuteError)
+            it 'is expected to checkout the repository' do
+              Dir.expects(:exist?).with(directory).twice.returns(true)
+              Dir.expects(:exists?).with("#{directory}/.svn").returns(true)
+              FileUtils.expects(:remove_entry_secure).with(directory, true)
+              subject.class.expects(:checkout).with(address, directory)
+
+              subject.class.retrieve!(address, directory, nil)
             end
           end
         end
       end
 
       context 'and it is not a svn repository' do
-        let!(:svn_checkout) { YAML.load_file('spec/factories/svn_downloader.yml')["svn_checkout"] }
-        let!(:command) { "svn checkout #{address} #{directory}" }
-
         it 'is expected to checkout the repository' do
-          subject.class.expects(:`).with(command).returns(svn_checkout)
+          subject.class.expects(:`).with(svn_checkout_command).returns(svn_checkout_result)
           Dir.expects(:exist?).with(directory).twice.returns(true)
           Dir.expects(:exists?).with("#{directory}/.svn").returns(false)
 
