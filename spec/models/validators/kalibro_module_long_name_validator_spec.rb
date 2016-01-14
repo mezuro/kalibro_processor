2c0ecda75686036a4a5b6feae9aab3e217b7801b
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2016-01-14 15:01:33 -0200

    Allow same name given different granularities
    It is possible to have same module names but different granularities.
    Take for example a directory with submodules and at same level of the
    directory a class with the same name.
    
    Specs were added for uncovered paths.
    
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- spec/models/validators/kalibro_module_long_name_validator_spec.rb
+++ spec/models/validators/kalibro_module_long_name_validator_spec.rb
@@ -8,19 +8,78 @@ RSpec.describe KalibroModuleLongNameValidator, :type => :model do
       let!(:module_result) { FactoryGirl.build(:module_result, kalibro_module: same_name) }
       let(:subject_module_result) { FactoryGirl.build(:module_result, processing: module_result.processing) }
       let(:same_name) { FactoryGirl.build(:kalibro_module) }
+      let(:processing) { FactoryGirl.create(:processing) }
 
-      context 'validating an unique long name within a processing' do
-        before do
-          subject.module_result = subject_module_result
-          subject_module_result.kalibro_module = subject
+      context 'within the same processing' do
+        context 'and with the same granularity' do
+          context 'and the same long name' do
+            before :each do
+              subject.module_result = subject_module_result
+              subject_module_result.kalibro_module = subject
 
-          same_name.save
-          module_result.save
+              same_name.save
+              module_result.save
+            end
+
+            it 'is expected to add an error to the record' do
+              subject.save
+              expect(subject.errors).not_to be_empty
+            end
+          end
+
+          context 'and a different long name' do
+            before :each do
+              subject.module_result = subject_module_result
+              subject_module_result.kalibro_module = subject
+            end
+
+            it 'is expected to NOT add any errors' do
+              subject.save
+              expect(subject.errors).to be_empty
+            end
+          end
         end
 
-        it 'is expected to add an error to the record' do
-          subject.save
-          expect(subject.errors).not_to be_empty
+        context 'with a different granularity' do
+          let(:granularity) { FactoryGirl.build(:class_granularity) }
+
+          context 'and the same long name' do
+            before :each do
+              subject.module_result = subject_module_result
+              subject_module_result.kalibro_module = subject
+              subject.granularity = granularity
+
+              same_name.save
+              module_result.save
+            end
+
+            it 'is expected to NOT add any errors' do
+              subject.save
+              expect(subject.errors).to be_empty
+            end
+          end
+        end
+      end
+
+      context 'within a different processing' do
+        let(:different_processing) { FactoryGirl.create(:processing) }
+
+        context 'and with the same granularity' do
+          context 'and the same long name' do
+            before :each do
+              subject_module_result.processing = different_processing
+              subject.module_result = subject_module_result
+              subject_module_result.kalibro_module = subject
+
+              same_name.save
+              module_result.save
+            end
+
+            it 'is expected to NOT add any errors' do
+              subject.save
+              expect(subject.errors).to be_empty
+            end
+          end
         end
       end
     end
