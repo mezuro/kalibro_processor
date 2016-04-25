4f5c1fe4c1e77ff4ccc589bc2eac3eb3e4202c7a
Diego Araújo <diegoamc@protonmail.ch>
2016-04-25 16:15:36 -0300

    Remove ruby analysis acceptance test
    kolekti_metricfu gem has a similar test. As this test is too slow, we
    are choosing to leave it just on the gem.

--- features/step_definitions/repository_steps.rb
+++ features/step_definitions/repository_steps.rb
@@ -1,7 +1,3 @@
 Given(/^I have the kalibro processor ruby repository with revision "(.+)"$/)do |tag|
   @repository = FactoryGirl.create(:repository, :kalibro_processor, branch: tag, kalibro_configuration: @configuration)
 end
-
-Given(/^I have Noosfero's ruby repository with branch "(.+)"$/)do |tag|
-  @repository = FactoryGirl.create(:repository, :noosfero, branch: tag, kalibro_configuration: @configuration)
-end
