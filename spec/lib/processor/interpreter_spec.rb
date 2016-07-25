f55b2653425e28596436e69c089e5802f24eab82
Daniel Miranda <danielkza2@gmail.com>
2016-07-25 01:17:24 -0300

    Refactor Interpreter processing step test
    In preparation for a rewrite of the interpreter itself. Test in a more
    comprehensive fashion by observing the result of the whole step in an
    actual ModuleResult tree with multiple native and compound metrics.

--- spec/lib/processor/interpreter_spec.rb
+++ spec/lib/processor/interpreter_spec.rb
@@ -2,53 +2,107 @@ require 'rails_helper'
 require 'processor'
 
 describe Processor::Interpreter do
-  describe 'methods' do
+  describe 'class methods' do
     describe 'task' do
-      context 'when the module result tree has been well-built' do
-        let(:kalibro_configuration) { FactoryGirl.build(:kalibro_configuration) }
-        let!(:code_dir) { "/tmp/test" }
-        let!(:repository) { FactoryGirl.build(:repository, scm_type: "GIT", kalibro_configuration: kalibro_configuration, code_directory: code_dir) }
-        let!(:metric_configuration) { FactoryGirl.build(:metric_configuration) }
-        let!(:metric_result) { FactoryGirl.build(:tree_metric_result,
-                                                 metric_configuration: metric_configuration) }
-        let!(:module_result) { FactoryGirl.build(:module_result_class_granularity,
-                                                 tree_metric_results: [metric_result]) }
-        let!(:processing) { FactoryGirl.build(:processing, repository: repository, root_module_result: module_result) }
-        let!(:context) { FactoryGirl.build(:context, repository: repository, processing: processing) }
+      let(:readings)            { [:bad, :average, :good].map { |kind| FactoryGirl.build(:reading, kind, :with_id) } }
+      let(:module_results_tree) { FactoryGirl.build(:module_results_tree, :with_id, height: 2, width: 10) }
+      let(:module_results)      { module_results_tree.all }
+      let(:processing)          { module_results_tree.root.processing }
+      let(:weights)             { (readings.size + 1).times.map { |i| rand(100) } }
+      let(:hotspot_metric)      { FactoryGirl.build(:hotspot_metric_configuration, :with_id) }
+      let(:native_metrics)      { tree_native_metrics + [hotspot_metric] }
+      let(:tree_native_metrics) do
+        # Create one extra metric to have it's result deliberately put outside of any of the ranges
+        (readings.size + 1).times.map { |i|
+          FactoryGirl.build(:metric_configuration, :with_id, :flog, weight: weights[i])
+        }
+      end
+      let(:compound_metrics) do
+        # Same as above, but make the weights different to avoid results matching by coincidence
+        (readings.size + 1).times.map { |i|
+          FactoryGirl.build(:compound_metric_configuration, :with_id, weight: weights[i] * 2)
+        }
+      end
+      let(:context) do
+        FactoryGirl.build(:context, processing: processing, compound_metrics: compound_metrics).tap do |context|
+          native_metrics.each do |mc|
+            context.native_metrics[mc.metric.metric_collector_name] << mc
+          end
+        end
+      end
 
+      def setup_metrics(metrics)
+        metrics.each_with_index do |mc, i|
+          # Create the ranges by partitioning the interval [0, readings.size), such that each reading's range has
+          # a length of 1 (the ranges are then [0, 1), [1, 2), [2, 3), ...)
+          ranges = (0..readings.size).each_cons(2).zip(readings).map do |(beg, end_), reading|
+            FactoryGirl.build(:range, beginning: beg, end: end_, metric_configuration_id: mc.id,
+                              reading_id: reading.id).tap do |range|
+              range.stubs(:reading).returns(reading)
+            end
+          end
+
+          KalibroClient::Entities::Configurations::KalibroRange.stubs(:ranges_of).with(mc.id).returns(ranges)
+
+          # The value for this metric result is the beginning of it's range, or -1 if it's meant to not be covered
+          # by any of the ranges
+          value_range = ranges.fetch(i, nil)
+          value = value_range ? value_range.beginning : -1
+
+          module_results.each do |module_result|
+            metric_result = module_result.tree_metric_results.build(metric_configuration_id: mc.id, value: value)
+            metric_result.stubs(:metric_configuration).returns(mc)
+          end
+        end
+      end
+
+      def self.it_is_expected_to_set_grades
+        it 'is expected to set the correct grade for every module result' do
+          numerator = denominator = 0
+          [tree_native_metrics, compound_metrics].each do |metrics|
+            metrics.zip(readings) do |mc, reading|
+              numerator += reading.nil? ? 0 : reading.grade * mc.weight
+              denominator += mc.weight
+            end
+          end
+
+          grade = numerator / denominator
+          module_results.each do |module_result|
+            module_result.expects(:update).with(grade: grade)
+          end
+
+          Processor::Interpreter.task(context)
+        end
+      end
+
+      context 'with tree metrics' do
         before :each do
-          module_result.expects(:children).returns([])
-        end
+          module_results_tree.root.expects(:pre_order).returns(module_results)
 
-        context 'when the module_result has a grade' do
-          let!(:range) { FactoryGirl.build(:range) }
-          let!(:reading) { FactoryGirl.build(:reading) }
-          let!(:weight) { metric_configuration.weight }
-          let!(:quotient) { (reading.grade * weight) / weight }
+          # Treat the tree native metrics and compound metrics as two distinct sets, each containing results for all
+          # the defined readings, and one result that doesn't match any reading.
+          setup_metrics(tree_native_metrics)
+          setup_metrics(compound_metrics)
 
-          before :each do
-            module_result.tree_metric_results.first.expects(:has_grade?).returns(true)
-            module_result.tree_metric_results.first.expects(:range).returns(range)
-            module_result.expects(:update).with(grade: quotient)
-            metric_result.expects(:metric_configuration).returns(metric_configuration)
-            range.expects(:reading).returns(reading)
-          end
-
-          it 'is expected to interpret, updating grade to quotient' do
-            Processor::Interpreter.task(context)
+          # Add hotspot metric results to make sure they are ignored as they should.
+          module_results.each do |module_result|
+            metric_result = module_result.hotspot_metric_results.build(metric_configuration_id: hotspot_metric.id)
+            metric_result.stubs(:metric_configuration).returns(hotspot_metric)
           end
         end
 
-        context 'when the module_result does not have a grade' do
-          before :each do
-            metric_result.expects(:metric_configuration).returns(metric_configuration)
-            module_result.tree_metric_results.first.expects(:has_grade?).returns(false)
-            module_result.expects(:update).with(grade: 0)
-          end
+        context 'with native and compound metrics' do
+          it_is_expected_to_set_grades
+        end
 
-          it 'is expected to interpret, updating grade to 0' do
-            Processor::Interpreter.task(context)
-          end
+        context 'with native metrics only' do
+          let(:compound_metrics) { [] }
+          it_is_expected_to_set_grades
+        end
+
+        context 'with compound metrics only' do
+          let(:tree_native_metrics) { [] }
+          it_is_expected_to_set_grades
         end
       end
     end
